8e63a9c596d373214c01341999c98ff4e4dbe4c6
Administrator <1516499114@qq.com>
2018-09-03 17:51:08 +0800

    作业

---
+++ python全栈/week1/day1/作业1.6.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/3 0003 16:08
+# @Author  : BigC
+# @Site    : 
+# @File    : 作业1.6.py
+# @Software: PyCharm
+age=int(input("请输入你的年龄:"))
+if age>90:
+    print("再见了这个世界.")
+elif age>70:
+    print("人生就快结束了的一个老老屁孩儿")
+elif age>60:
+    print("活着还不不错的老屁孩儿")
+elif age>50:
+    print("自己马上变成不不听话的老屁孩儿")
+elif age>40:
+    print("家里里有个不听话的小屁孩儿")
+elif age>30:
+    print("老大不不小了了, 赶紧结婚小屁孩儿")
+elif age>20:
+    print("开始定性, 开始混社会的小屁孩儿")
+elif age>10:
+    pritn("青春期叛逆的小屁孩")
+else:
+    print("小屁孩")
